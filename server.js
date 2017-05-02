06d35ba9e2645863462aebb6bcd0b310944b352a
Marissa Reinke <reinke.marissa@gmail.com>
2017-05-02 15:11:31 -0600

    Setup server file with GET and POST for folders

--- server.js
+++ server.js
@@ -1,16 +1,60 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const md5 = require('md5');
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'JetFuel';
+
+app.locals.folders = {};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-   res.sendFile('index.html');
-})
+app.get('/', (request, response ) => {
+  fs.readFile(`${__dirname}/index.html`, (err, file) => {
+    response.send(file)
+  })
+});
 
-var server = app.listen(3000, function () {
-   var host = server.address().address
-   var port = server.address().port
+app.get('/api/folders', (request, response) => {
+  const folders = app.locals.folders;
+  response.json({ folders })
+});
 
-   console.log("Example app listening at http://%s:%s", host, port)
+app.post('/api/folders', (request, response) => {
+  const { title } = request.body;
+  const id = md5(title);
 
-})
+  if (!title) {
+    return response.status(422).send({
+      error: 'No Folder Title was provided.'
+    })
+  }
+
+  app.locals.folders[id] = title
+  response.status(201).json({ id, title })
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+});
+
+// var express = require('express');
+// var app = express();
+//
+// app.use(express.static('public'));
+//
+// app.get('/', function (req, res) {
+//    res.sendFile('index.html');
+// })
+//
+// var server = app.listen(3000, function () {
+//    var host = server.address().address
+//    var port = server.address().port
+//
+//    console.log("Example app listening at http://%s:%s", host, port)
+//
+// })
