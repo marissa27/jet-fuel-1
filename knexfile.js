e74aa85120cb8ef8c29f2219f13be37718b7d2f5
Marissa Reinke <reinke.marissa@gmail.com>
2017-05-05 10:24:39 -0600

    initial heroku

--- knexfile.js
+++ knexfile.js
@@ -13,6 +13,14 @@ module.exports = {
     },
     useNullAsDefault: true
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  },
   test: {
     client: 'pg',
     connection: 'postgres://localhost/jetdbtest',
